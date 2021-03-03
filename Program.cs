50b25e8be69a2197d3ae582458e0a28e3e4f9b31
alanisaias24147 <78932267+alanisaias24147@users.noreply.github.com>
2021-03-03 02:26:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[100];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+
+
+                int secuencia = i + 1; 
+
+                if (secuencia % 15 == 0) 
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if (secuencia % 5 == 0) 
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else if (secuencia % 3 == 0)
+                {
+                    Console.WriteLine("Fizz"); 
+                }
+
+                else  
+
+                {
+                    Console.WriteLine(secuencia);
+                }
+
+                
+            }
+        }
+    }
+}
+      
